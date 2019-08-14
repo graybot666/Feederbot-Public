2a46997002a9e059564f45ee57f891d27e102b90
graybot666 <graybot666@gmail.com>
2019-08-14 09:56:25 +0530

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ var app = angular.module('myApp', []);
 
 app.controller('userCtrl', function($scope, TwitterGetUserService){
 	$scope.getUser = function(username){
-		//console.log("username entered ", username);
 		TwitterGetUserService.getUser(username)
 		    .then(function(data){
 		        $scope.twitterErrors = undefined;
@@ -16,17 +15,78 @@ app.controller('userCtrl', function($scope, TwitterGetUserService){
 });
 
 app.controller('tweetCtrl', function($scope, TwitterGetTweetsService){
-	$scope.getTweets = function(username){
-		//console.log("username entered ", username);
-		TwitterGetTweetsService.getTweets(username)
-		    .then(function(data){
-				$scope.twitterErrors = undefined;
-	        	$scope.tweets = JSON.parse(data.result.tweetData);
-		    })
-		    .catch(function(error){
-		        console.error('there was an error retrieving data: ', error);
-		        $scope.twitterErrors = error.error;
-		    })
+	$scope.refreshTime=null;
+	$scope.tweets=[];
+	$scope.handlesWatch=[];
+	$scope.readTweetsID=[];
+	$scope.addHandleToWatch = function(username){
+		$scope.handlesWatch.push(username);
+		//window.localStorage.setItem('handles',$scope.handlesWatch);
+		$scope.pullnewTweets(username);
+	}
+	$scope.startCountDown = function(){
+		setInterval(function(){$scope.refreshTime-=1;}, 1000);
+	}
+	$scope.startPolling = function(){
+		setInterval(function(){ $scope.pullallTweets() }, 30000);
+	}
+	$scope.pullallTweets = function(){
+		$scope.handlesWatch.forEach(handle => {
+			// console.log('Polling : '+handle);
+			$scope.pullnewTweets(handle);
+		});
+	}
+	$scope.pullnewTweets = function(handle){
+		TwitterGetTweetsService.getTweets(handle)
+		.then(function(data){
+			$scope.twitterErrors = undefined;
+			var newTweets = JSON.parse(data.result.tweetData);
+			if ($scope.readTweetsID.length===0){
+				newTweets.forEach(el=>{
+					$scope.tweets.push(el);
+				});
+			} else if ($scope.readTweetsID.length>=1){
+				let sanitizedData=$scope.sanitizeData( newTweets );
+				sanitizedData.forEach(el => {
+					$scope.tweets.push(el);
+				});
+			}
+		})
+		.catch(function(error){
+			console.error('there was an error retrieving data: ', error);
+			$scope.twitterErrors = error.error;
+		})
+	}
+	$scope.sanitizeData = function(data){
+		//Filter for tweets that already exist in DOM
+		for (let i = 0; i < data.length; i++) {
+			const tweetID = data[i].id_str;
+			$scope.tweets.forEach(el => {
+				if (el.id_str===tweetID){
+					data.splice(i,1);
+					i-=1;
+				}
+			});
+		}
+		//Filter for unread tweets
+		for (let i = 0; i < data.length; i++) {
+			const tweetID = data[i].id_str;
+			$scope.readTweetsID.forEach(el => {
+				if (el===tweetID){
+					data.splice(i,1);
+					i-=1;
+				}
+			});
+		}
+		return data;
+	}
+	$scope.markRead = function(iD){
+		let removedTweet=$scope.tweets.splice(iD,1);
+		$scope.addtoLocalStorage(removedTweet);
+	}
+	$scope.addtoLocalStorage = function(tweet){
+		$scope.readTweetsID.push(tweet[0].id_str);
+		//window.localStorage.setItem('readTweets',$scope.readTweets);
 	}
 });
 
@@ -67,3 +127,4 @@ app.factory('TwitterGetTweetsService', function($http, $q){
     getTweets : getTweets
   }
 });
+
